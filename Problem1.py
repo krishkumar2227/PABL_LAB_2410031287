840f65d0334e57e63902c57284e63bcc991909d9
krishkumar2227 <krishkumar.200622@gmail.com>
2026-04-01 23:36:28 +0530

    PROBLEM 1

---
+++ Problem1.py
@@ -0,0 +1,18 @@
+#1.You are given an array of integers arr[]. You have to reverse the given array.
+def reverse_array(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+    return arr
+
+
+
+arr = [1, 2, 3, 4, 5]
+reverse_array(arr)
+print(arr)
