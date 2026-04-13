f5c9a5d99cffe73987d3388abcd99beb7084f2d9
krishkumar2227 <krishkumar.200622@gmail.com>
2026-04-13 23:40:55 +0530

    PROBLEM 32

---
+++ Problem32.py
@@ -0,0 +1,24 @@
+#32.You are given an m x n integer matrix matrix with the following two properties:
+#• Each row is sorted in non-decreasing order.
+#• The first integer of each row is greater than the last integer of the previous row.
+#Given an integer target, return true if target is in matrix or false otherwise.
+#You must write a solution in O(log(m * n)) time complexity.
+def searchMatrix(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+
+    m, n = len(matrix), len(matrix[0])
+    left, right = 0, m * n - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        value = matrix[mid // n][mid % n]
+
+        if value == target:
+            return True
+        elif value < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
