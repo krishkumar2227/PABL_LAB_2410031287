67cfa8fd4b73defeffad0e0cdc0ad5957fed723f
krishkumar2227 <krishkumar.200622@gmail.com>
2026-05-06 23:08:21 +0530

    PROBLEM 41

--- Problem41.py
+++ Problem41.py
@@ -1,1 +1,8 @@
-"Hello World"
+#41.Print Factorial of each number from 1 to n.
+n = int(input("Enter a number: "))
+
+fact = 1
+
+for i in range(1, n + 1):
+    fact = fact * i
+    print("Factorial of", i, "=", fact)
