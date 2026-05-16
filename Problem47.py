5e5877048fb61e6f5ae2dc8d0e7b419442a455fd
krishkumar2227 <krishkumar.200622@gmail.com>
2026-05-16 23:33:04 +0530

    PROBLEM 47

---
+++ Problem47.py
@@ -0,0 +1,9 @@
+#47.Swap the first and last elements of the array.
+# Swap first and last elements of an array
+
+arr = [10, 20, 30, 40, 50]
+
+# Swapping
+arr[0], arr[-1] = arr[-1], arr[0]
+
+print("Array after swapping:", arr)
