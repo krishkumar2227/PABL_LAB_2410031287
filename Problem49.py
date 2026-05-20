9670ea7a43093e40ce280c2c1aa391642a2111e0
krishkumar2227 <krishkumar.200622@gmail.com>
2026-05-20 21:11:12 +0530

    PROBLEM 49

--- Problem49.py
+++ Problem49.py
@@ -1,1 +1,25 @@
-/..
+#49.Move All Zeros to End
+def move_zeros(arr):
+    index = 0
+
+    # Move non-zero elements forward
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[index] = arr[i]
+            index += 1
+
+    # Fill remaining positions with 0
+    while index < len(arr):
+        arr[index] = 0
+        index += 1
+
+    return arr
+
+
+# Example
+arr = [1, 0, 2, 0, 4, 0, 5]
+
+result = move_zeros(arr)
+
+print("Array after moving zeros:")
+print(result)
