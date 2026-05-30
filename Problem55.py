557bde5b60a20c026511df51cc0a495a91a21ce2
krishkumar2227 <krishkumar.200622@gmail.com>
2026-05-30 21:19:05 +0530

    PROBLEM 55

---
+++ Problem55.py
@@ -0,0 +1,11 @@
+#55.class Solution:
+def lengthOfLIS(self, nums):
+        n = len(nums)
+        dp = [1] * n
+
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
