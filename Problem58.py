bc979efea8fd19c910c388f1306b22254d10dd57
krishkumar2227 <krishkumar.200622@gmail.com>
2026-06-06 22:18:58 +0530

    PROBLEM 58

---
+++ Problem58.py
@@ -0,0 +1,13 @@
+#58.Group Anagrams.
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for s in strs:
+            key = ''.join(sorted(s))
+            groups[key].append(s)
+
+        return list(groups.values())
